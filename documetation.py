51af315f076e4a0aa07141f2bf9d8543ca2fd057
Nazar Rudenok <122445773+nazarrudenok@users.noreply.github.com>
2023-02-08 14:18:39 +0200

    Add files via upload

---
+++ documetation.py
@@ -0,0 +1,30 @@
+# import LossesStaisticsOfrussia as losses
+
+# losses.date #повертає дату оновлення інформації
+
+# losses.personnel  #повертає інформацію про втрати особового складу
+# losses.tanks  #повертає інформацію про кількість знищених танків
+# losses.bbm  #повертає інформацію про кількість знищеної бойової бронетехніки
+# losses.planes  #повертає інформацію про кількість знищених літаків
+# losses.helicopters  #повертає інформацію про кількість знищених гелікоптерів
+# losses.guns  #повертає інформацію про кількість знищених гармат
+# losses.ppo_means  #повертає інформацію про кількість знищених засобів ППО
+# losses.drones  #повертає інформацію про кількість знищених безпілотників
+# losses.cars  #повертає інформацію про кількість знищених автомобілів
+# losses.rockets  #повертає інформацію про кількість знищених крилатих ракет
+
+
+import LossesStaisticsOfrussia as losses
+
+losses.date #returns the information update date
+
+losses.personnel  #returns information about personnel losses
+losses.tanks  #returns information about the number of destroyed tanks
+losses.bbm  #returns information about the number of destroyed combat armored vehicles
+losses.planes  #returns information about the number of destroyed aircraft
+losses.helicopters  #returns information about the number of helicopters destroyed
+losses.guns  #returns information about the number of destroyed guns
+losses.ppo_means  #returns information about the number of destroyed anti-aircraft vehicles
+losses.drones  #returns information about the number of destroyed drones
+losses.cars  #returns information about the number of destroyed cars
+losses.rockets  #returns information about the number of destroyed cruise missiles
